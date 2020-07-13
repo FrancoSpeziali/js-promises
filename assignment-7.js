8b38572e84dfd3464b767ab69652625bbd8253e6
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2020-07-13 11:04:10 +0200

    Added project files

---
+++ assignment-7.js
@@ -0,0 +1,13 @@
+/*
+
+Assignment 7 - Creating an error
+
+In this exercise, you will be creating an Error object
+
+1. Instantiate the Error object using the "new" keyword. Use the message "No graphic was provided" in the Error.
+
+2. Test your code to see what happens
+
+*/
+
+
