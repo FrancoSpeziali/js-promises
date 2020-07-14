01fb3cf57b3ed4c68053ac9ddc8aa593d7d23fec
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2020-07-14 13:51:19 +0200

    Fixed assignment 7,8 wording

--- assignment-8.js
+++ assignment-8.js
@@ -6,7 +6,7 @@ Below you will find a function call which expects a Promise to be returned
 
 You will also find the function "loadGraphic" has been prepared for you
 
-1. On line 16, inside the Promise callback check if the "graphic" argument is available. If it is not, reject it with the reject() method
+1. On line 20, inside the Promise callback check if the "graphic" argument is available. If it is not, reject it with the reject() method
 
 2. Instantiate the Error object using the "new" keyword. Use the message "No graphic was provided" in the Error.
 
